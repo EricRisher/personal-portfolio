738fdabc0c67a61ac744bb33c8805605f6981e35
EricRisher <rishereric13@gmail.com>
2023-09-21 22:32:59 -0700

    fixed functionality to better fit requirements

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let button = document.getElementById('btn');
-
-let includeUpper   = confirm('Do you want upper case letters?');
-let includeLower   = confirm("Do you want lower case letters?");
-let includeNums    = confirm("Do you want numbers?");
-let includeSpecial = confirm("Do you want special characters?");
-let passwordLength = prompt( "Choose a password length between 8 and 128 characters.");
-let passLength     = parseInt(passwordLength);
-
 button.addEventListener("click", generatePassword);
 
-if (passLength >= 1 && passLength <= 50) {
+
   function generatePass() {
     let passwordStr = '';
     let UPPERCASE_CHAR = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let LOWERCASE_CHAR = "abcdefghijklmnopqrstuvwxyz";
     let NUMBER_CHAR = "0123456789";
     let SYMBOL_CHAR = "!@#$%^&*(){}[];':<>?|";
+    let includeUpper = confirm("Do you want upper case letters?");
+    let includeLower = confirm("Do you want lower case letters?");
+    let includeNums = confirm("Do you want numbers?");
+    let includeSpecial = confirm("Do you want special characters?");
+    let passwordLength = prompt("Choose a password length");
+let passLength = parseInt(passwordLength);
 
     if (includeUpper === true) {
       passwordStr += UPPERCASE_CHAR
@@ -36,13 +34,10 @@ if (passLength >= 1 && passLength <= 50) {
     }
     return password;
   }
-} else {
-  alert("Password must be inbetween 8 and 128 characters!");
-}
 
 function generatePassword(){
             let password = generatePass();
             let passwordEl = document.getElementById("passwordDisplay");
 
             passwordEl.value = password;
-        }
+}
